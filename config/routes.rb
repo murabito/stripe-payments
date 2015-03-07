2968aa2d868660c33fb6d36c6a0cc45cbbb7c313
Christine Murabito <christine.murabito@gmail.com>
2015-03-07 13:04:07 -0500

    Implemented first cut of the charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  #Support stripe payments through charges
+  resources :charges
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
